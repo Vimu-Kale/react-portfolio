892f674e54405bf2774f7797beed724370faa520
VimuKale <vimurk111@gmail.com>
2022-04-12 15:25:03 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Hero/>
-      
+        <Routes>
+          <Route path='/' element={<Hero/>} />
+          <Route path='/skills' element={<Skills/>} />
+          <Route path='/projects' element={<Projects/>} />
+        </Routes> 
+      <Footer/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,17 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-  };
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+
+  const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
@@ -30,44 +25,25 @@ const Header = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup>
+      
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Photos
           </Typography>
-    
+        
             <div>
               <IconButton
                 size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
+                edge="start"
                 color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={handleMenu}
               >
-                <AccountCircle />
+                <MenuIcon />
               </IconButton>
+
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorEl}
@@ -83,14 +59,17 @@ const Header = () => {
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
               >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}><Link to="/">Home</Link></MenuItem>
+                <MenuItem onClick={handleClose}><Link to="/skills">Skills</Link></MenuItem>
+                <MenuItem onClick={handleClose}><Link to="/projects">Projects</Link></MenuItem>
               </Menu>
             </div>
+          
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
 
 export default Header;
